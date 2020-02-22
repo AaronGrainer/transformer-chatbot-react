4ec99fbf5e82b738ceefbb20cfc9a102e99a8b80
Aaron Grainer <aaronlimfz@hotmail.com>
2020-02-22 18:27:18 +0800

    Added simple popup chat window

--- src/views/Chatbot/ChatWindow.js
+++ src/views/Chatbot/ChatWindow.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
+import {
+  Widget,
+  addResponseMessage,
+  addLinkSnippet,
+  addUserMessage
+} from "react-chat-widget"
+
+import "react-chat-widget/lib/styles.css"
+import logo from "../../logo.svg"
 
 class ChatWindow extends Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
 
-  
+  handleNewUserMessage = newMessage => {
+    addResponseMessage("Welcome to this awesome chat!")
+  }
 
   render() {
     return (
-      <div>
-        
+      <div className="App">
+        <Widget
+          handleNewUserMessage={this.handleNewUserMessage}
+          profileAvatar={logo}
+          title="Transformer"
+          subtitle="GPT-2"
+        />
       </div>
-    );
+    )
   }
 }
 
